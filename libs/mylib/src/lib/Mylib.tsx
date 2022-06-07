498f121fd662e2502155f08e7a23421a91bfb01a
Jason Bright <jgbright@gmail.com>
2022-06-07 10:25:54 -0400

    add lib code that requires tsconfig changes
    this code will only compile if the `noImplicitReturns` tsconfig option is false

--- libs/mylib/src/lib/Mylib.tsx
+++ libs/mylib/src/lib/Mylib.tsx
@@ -7,9 +7,20 @@ const StyledMylib = styled.div`
   color: pink;
 `;
 
+const getSomeText = (capitalized?: boolean) => {
+  if (!capitalized) {
+    return 'some text';
+  }
+  // return undefined;
+  // Depending on the tsconfig, we may get an error here: `TS7030: Not all code paths return a value.`
+};
+
 export function Mylib(props: MylibProps) {
   return (
     <StyledMylib>
+      <p>
+        Here is some text: <code>{getSomeText()}</code>
+      </p>
       <h1>Welcome to Mylib!</h1>
     </StyledMylib>
   );
